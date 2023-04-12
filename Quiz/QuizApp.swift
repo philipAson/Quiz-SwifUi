04b832a04ef51dd772f600ffaab7ab7d4097c3d0
Philip Andersson <philip.andersson.1991@hotmail.com>
2023-04-12 09:14:30 +0200

    Initial Commit

---
+++ Quiz/QuizApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuizApp.swift
+//  Quiz
+//
+//  Created by Philip Andersson on 2023-04-12.
+//
+
+import SwiftUI
+
+@main
+struct QuizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
